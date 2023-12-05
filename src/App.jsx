78db6bd832348993cefee91ec7aef4fc019ca3c8
Sujata <076bct085.sujata@pcampus.edu.np>
2023-12-05 21:57:19 +0545

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Carousel from './components/Carousel/Carousel'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <Carousel />
+      {/* <Carousel /> */}
+      <Navbar/>
        
     </>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import "./navbar.css";
+import logo from "./logo.png";
+import navelements from "./const.Navbar";
+import { faBurger } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Navbar() {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+
+    return (
+        <div className="nav-container">
+          <img className="nav-logo" src={logo} alt="logo" />
+          <div className={clicked ? "nav-menu active" : "nav-menu"}>
+            {navelements.map((element) => (
+              <NavLink key={element.id} to={element.path} className="nav-element">
+                {element.name}
+              </NavLink>
+            ))}
+          </div>
+          <div className="nav-burger" onClick={handleClick}>
+            <FontAwesomeIcon icon={faBurger} />
+          </div>
+        </div>
+      );
+      
+    }
